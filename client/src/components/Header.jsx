e6db65cc1119bca3d86d78361599abde09fae348
VENKATA VASU NALLAMEKALA <nvvasu7804@gmail.com>
2025-01-21 21:19:22 +0530

    moved ul section of header from form section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -20,26 +20,26 @@ export default function Header() {
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
           <FaSearch className="text-slate-600 " />
-          <ul className="flex gap-4">
-            <Link to="/">
-              <li className="hidden sm:inline text-slate-700 hover:underline">
-                Home
-              </li>
-            </Link>
-
-            <Link to="/about">
-              <li className="hidden sm:inline text-slate-700 hover:underline">
-                About
-              </li>
-            </Link>
-
-            <Link to="/sign-in">
-              <li className="sm:inline text-slate-700 hover:underline">
-                Sign In
-              </li>
-            </Link>
-          </ul>
         </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+
+          <Link to="/sign-in">
+            <li className="sm:inline text-slate-700 hover:underline">
+              Sign In
+            </li>
+          </Link>
+        </ul>
       </div>
     </header>
   );
